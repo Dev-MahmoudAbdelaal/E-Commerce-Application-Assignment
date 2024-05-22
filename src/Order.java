7c96a7aae09b9f528c304eeccd499ca219613a1f
Mahmoud Abdelaal <mahmoud.abdelaal.dev@gmail.com>
2024-05-22 17:39:46 +0300

    Order Model

--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class Order {
+import java.util.List;
+
+public class Order {
+    private int id;
+    private Customer customer;
+    private List<Product> productList;
+    private double totalAmount;
+
+    public Order(int id, Customer customer, List<Product> productList) {
+        this.id = id;
+        this.customer = customer;
+        this.productList = productList;
+        this.totalAmount=calculateTotalAmount();
+    }
+    public double calculateTotalAmount(){
+        double total=0;
+        //Enhance the loop to iterate over each product in the product list, retrieving the price of each product, and summing them up.
+        for (Product product:productList){
+            total+=product.getPrice();
+        }
+        return total;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", customer=" + customer +
+                ", productList=" + productList +
+                ", totalAmount=" + totalAmount +
+                '}';
+    }
 }
