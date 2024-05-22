4a6229133c7965385883cbb4a01c580d9dfee19b
Mahmoud Abdelaal <mahmoud.abdelaal.dev@gmail.com>
2024-05-22 17:48:06 +0300

    Implemented product controller functionality

--- src/ProductManager.java
+++ src/ProductManager.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class productManager {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductManager {
+   private List<Product> productList;
+    public ProductManager(){
+        this.productList=new ArrayList<>();
+    }
+    public void addProduct(Product product){
+
+        boolean exists = false;
+        for (Product prod : productList) {
+            if (prod.getId() == product.getId()) {
+                updateStock(product.getId(), product.getStockQuantity());
+                exists = true;
+                break;
+            }
+        }
+        if (!exists) {
+            productList.add(product);
+        }
+    }
+
+    public void removeProduct(int productId){
+        for (Product product:productList){
+            if (productId==product.getId()){
+                productList.remove(product);
+                break;
+            }
+        }
+    }
+
+    public void viewAllProducts(){
+        for (Product product:productList){
+            System.out.println(product);
+        }
+    }
+    public void updateStock(int productId, int newStock) {
+        for (Product product : productList) {
+            if (product.getId() == productId) {
+                product.setStockQuantity(newStock);
+
+                break;
+            }
+        }
+    }
+    public List<Product> getAllProducts(){
+        return productList;
+    }
+
+
+
+
 }
