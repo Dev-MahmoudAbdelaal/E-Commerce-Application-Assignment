0da81cede23ebfe41e356b3b25c15420663a0e70
Mahmoud Abdelaal <mahmoud.abdelaal.dev@gmail.com>
2024-05-22 17:45:36 +0300

    Implemented order controller functionality

--- src/OrderManager.java
+++ src/OrderManager.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class OrderManager {
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderManager {
+    List<Order> orderList;
+
+    public OrderManager() {
+        this.orderList = new ArrayList<>();
+    }
+
+    public void addOrder(Order order){
+        orderList.add(order);
+    }
+    public void removeOrder(int orderId){
+        for (Order order:orderList){
+            if(orderId==order.getId()){
+                orderList.remove(order);
+                break;
+            }
+
+        }
+    }
+    public void viewAllOrders(){
+        for (Order order:orderList){
+            System.out.println(order);
+        }
+    }
+
+
 }
